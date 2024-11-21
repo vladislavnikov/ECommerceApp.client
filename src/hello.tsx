4e7433c423e5dfff27ba14334e96174b529e0e91
Vladislav <vladislav.d.nikov04@gmail.com>
2024-11-21 16:07:41 +0200

    Create Hello page

---
+++ src/hello.tsx
@@ -0,0 +1,2 @@
+const Hello = () => <h1>Hello, React + TypeScript!</h1>;
+export default Hello;
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import ReactDOM from "react-dom/client";
 import TheHeader from "./components/theHeader";
 import Login from "./components/account/login";
 import apiEndpoints from "./api.endpoints";
+import Hello from "./Hello";
 
 interface Props {}
 interface State {}
