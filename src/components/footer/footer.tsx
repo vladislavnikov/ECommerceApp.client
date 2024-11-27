a09a02f8d3dedd160338005ecd92562270c311b7
Vladislav <vladislav.d.nikov04@gmail.com>
2024-11-27 16:18:02 +0200

    Add footer
    Implement footer component and scss module for its style.

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./footer.module.scss";
+
+function Footer(): React.ReactElement {
+  return (
+    <footer className={styles.footer}>
+      <p>Incredible convenient</p>
+      <div className={styles.icons}>
+        <a href="https://www.company1.com" target="_blank" rel="noopener noreferrer">
+          <img alt="Company 1" />
+        </a>
+        <a href="https://www.company2.com" target="_blank" rel="noopener noreferrer">
+          <img alt="Company 2" />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
