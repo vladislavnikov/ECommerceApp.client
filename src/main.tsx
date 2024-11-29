976405d6c164427dc240efb13fdda0929f7835aa
vladislavnikov <vladislav.d.nikov04@gmail.com>
2024-11-29 13:23:07 +0200

    Implement errorBoundary component

--- src/main.tsx
+++ src/main.tsx
@@ -1,47 +1,28 @@
 import "./styles/main.scss";
-import { Component, ErrorInfo /* , StrictMode */ } from "react";
 import ReactDOM from "react-dom/client";
+import { Component } from "react";
+import ErrorBoundary from "src/components/errorBoundary";
 import apiEndpoints from "./api.endpoints";
 import App from "./app";
-
-interface Props {}
-interface State {
-  hasError: boolean;
-}
 
 async function testFetch(): Promise<void> {
   const data = await (await fetch(apiEndpoints.testMock)).json();
   console.warn("fetched data", data);
 }
 
-class AppContainer extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { hasError: false };
-
-    const goExclude = true;
-    if (!goExclude) {
-      console.warn("class-dead-code doesn't work", props);
-    }
-  }
-
+class AppContainer extends Component {
   componentDidMount(): void {
     setTimeout(testFetch, 300);
   }
 
-  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-    console.error("got err", { error, errorInfo });
-    this.setState({ hasError: true });
-  }
-
   render() {
-    // <StrictMode>
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
-    }
-
-    return <App />;
-    // </StrictMode>
+    return (
+      // <StrictMode>
+      <ErrorBoundary fallback={<h1>Something went wrong! Please try again later.</h1>}>
+        <App />
+      </ErrorBoundary>
+      // <StrictMode />
+    );
   }
 }
 
