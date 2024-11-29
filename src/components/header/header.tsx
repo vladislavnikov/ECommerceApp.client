5ca6f0b62f4c851d0d518078585cd13b28a07bcf
vladislavnikov <vladislav.d.nikov04@gmail.com>
2024-11-29 17:22:17 +0200

    Implement Products dropdown

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import ROUTES from "src/constants/routes";
 import * as styles from "./header.module.scss";
 
 function Header() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Games Store</h1>
@@ -10,9 +13,24 @@ function Header() {
         <NavLink to={ROUTES.HOME} className={({ isActive }) => (isActive ? styles.active : "")}>
           Home
         </NavLink>
-        <NavLink to={ROUTES.PRODUCTS} className={({ isActive }) => (isActive ? styles.active : "")}>
-          Products
-        </NavLink>
+        <div className={styles.dropdown} onMouseEnter={() => setDropdownOpen(true)} onMouseLeave={() => setDropdownOpen(false)}>
+          <NavLink to={ROUTES.PRODUCTS} className={({ isActive }) => `${styles.dropbtn} ${isActive ? styles.active : ""}`}>
+            Products
+          </NavLink>
+          {isDropdownOpen && (
+            <div className={styles.dropdownContent}>
+              <NavLink to="/categories/pc" className={({ isActive }) => (isActive ? styles.active : "")}>
+                PC
+              </NavLink>
+              <NavLink to="/categories/playstation" className={({ isActive }) => (isActive ? styles.active : "")}>
+                Playstation 5
+              </NavLink>
+              <NavLink to="/categories/xbox" className={({ isActive }) => (isActive ? styles.active : "")}>
+                Xbox One
+              </NavLink>
+            </div>
+          )}
+        </div>
         <NavLink to={ROUTES.ABOUT} className={({ isActive }) => (isActive ? styles.active : "")}>
           About
         </NavLink>
