2b12dcf36f6ae3141b991b748eba83214e8152e2
vladislavnikov <vladislav.d.nikov04@gmail.com>
2025-01-13 09:53:45 +0200

    Remove file

--- src/utility/fetchWithDelay.ts
+++
@@ -1,8 +0,0 @@
-const fetchWithDelay = <T>(fetcher: () => Promise<T>, delay = 500): Promise<T> =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      fetcher().then(resolve).catch(reject);
-    }, delay);
-  });
-
-export default fetchWithDelay;
